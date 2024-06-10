ccba2beab31c5bedf8711d834433f6b03dcdebb2
ornchannlyhov <159111533+ornchannlyhov@users.noreply.github.com>
2024-06-10 22:56:53 +0700

    change to view

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -2,21 +2,38 @@
 <html>
 <head>
     <title>Show Record</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .btn-group {
+            margin-top: 10px;
+        }
+
+        /* Style to keep the navbar fixed at the top */
+        .navbar-fixed {
+            position: fixed;
+            width: 100%;
+            top: 0;
+            z-index: 1000;
+        }
+
+        /* Add padding/margin between navbar and table */
+        .container {
+            margin-top: 70px; /* Adjust this value according to your preference */
+        }
+    </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top z-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
         <div class="container-fluid">
-        <a class="navbar-brand h1" href={{ route('record.index') }}>Records</a>
-        <div class="justify-end ">
-            <div class="col ">
-            <a class="btn btn-sm btn-success" href={{ route('record.create') }}>Add Record</a>
+            <a class="navbar-brand h1" href="{{ route('record.index') }}">Records</a>
+            <div class="justify-end ">
+                <div class="col">
+                    <a class="btn btn-sm btn-success" href="{{ route('record.create') }}">Add Record</a>
+                </div>
             </div>
         </div>
-        </div>
     </nav>
-    <div class="container " style="margin-top:5%">
+    <div class="container">
         @foreach ($record as $info )
             <h3>{{$info->name}}</h3>
             <p>job: {{ $info->job }}</p>
